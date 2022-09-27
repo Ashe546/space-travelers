fe00d04c6dccd66cb21364c703913d3748adc8ac
Ashe546 <asfyd42@gmail.com>
2022-09-27 10:12:35 -0300

    Added table to mission component

---
+++ src/components/Mission/Mission.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Mission.module.css'
+
+export default function Mission() {
+  return (
+    <div >
+<table className={styles.container}>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+    </div>
+  )
+}
